42afe897ad3b53afc21af5c13c556e3305a8e06e
Snowsita <Snowsita@users.noreply.github.com>
2026-02-13 22:47:13 -0600

    feat(util): Created util file to cleanup code

--- app/main.go
+++ app/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/chzyer/readline"
 	"github.com/codecrafters-io/shell-starter-go/app/shell"
 	"os"
-	"os/exec"
 	"strings"
 )
 
@@ -68,69 +67,5 @@ func main() {
 		} else {
 			runSingleCommand(&history, parts)
 		}
-	}
-}
-
-func runSingleCommand(history *[]string, parts []string) {
-	command := parts[0]
-	info := shell.ParseRedirections(parts[1:])
-
-	switch command {
-	case "exit":
-		os.Exit(0)
-	case "echo":
-		shell.HandleEcho(info, os.Stdout)
-	case "type":
-		shell.HandleType(info, os.Stdout, getExecutablePath)
-	case "pwd":
-		shell.HandlePwd(info, os.Stdout)
-	case "cd":
-		if err := shell.HandleCd(info.FinalArgs); err != nil {
-			fmt.Fprintf(os.Stderr, "cd: %s: No such file or directory\n", info.FinalArgs[0])
-		}
-	case "history":
-		shell.HandleHistory(history, info, os.Stdout)
-	default:
-		fullPath := getExecutablePath(command)
-
-		if fullPath != "" {
-			cmd := exec.Command(fullPath, info.FinalArgs...)
-
-			cmd.Args[0] = command
-
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-
-			outWriter, _ := shell.GetOutputWriter(info.StdoutFile, false, os.Stdout)
-			if info.AppendFile != "" {
-				outWriter, _ = shell.GetOutputWriter(info.AppendFile, true, os.Stdout)
-			}
-			cmd.Stdout = outWriter
-
-			errWriter, _ := shell.GetOutputWriter(info.StderrFile, false, os.Stderr)
-			if info.AppendErrFile != "" {
-				errWriter, _ = shell.GetOutputWriter(info.AppendErrFile, true, os.Stderr)
-			}
-			cmd.Stderr = errWriter
-
-			cmd.Run()
-		} else {
-			fmt.Printf("%s: command not found\n", command)
-		}
-	}
-}
-
-func runBuiltin(history *[]string, name string, info shell.RedirectInfo, out *os.File) {
-	switch name {
-	case "echo":
-		shell.HandleEcho(info, out)
-	case "type":
-		shell.HandleType(info, out, getExecutablePath)
-	case "pwd":
-		shell.HandlePwd(info, out)
-	case "cd":
-	case "history":
-		shell.HandleHistory(history, info, out)
-	case "exit":
 	}
 }
---
+++ app/util.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codecrafters-io/shell-starter-go/app/shell"
+	"os"
+	"os/exec"
+)
+
+func runBuiltin(history *[]string, name string, info shell.RedirectInfo, out *os.File) {
+	switch name {
+	case "exit":
+		histFile := os.Getenv("HISTFILE")
+		if histFile != "" {
+			_ = shell.AppendHistory(history, histFile)
+		}
+		os.Exit(0)
+
+	case "echo":
+		shell.HandleEcho(info, out)
+
+	case "type":
+		shell.HandleType(info, out, getExecutablePath)
+
+	case "pwd":
+		shell.HandlePwd(info, out)
+
+	case "cd":
+		if err := shell.HandleCd(info.FinalArgs); err != nil {
+			fmt.Fprintf(os.Stderr, "cd: %s: No such file or directory\n", info.FinalArgs[0])
+		}
+
+	case "history":
+		shell.HandleHistory(history, info, out)
+	}
+}
+
+func runSingleCommand(history *[]string, parts []string) {
+	command := parts[0]
+	info := shell.ParseRedirections(parts[1:])
+
+	switch command {
+	case "exit", "echo", "type", "pwd", "cd", "history":
+		runBuiltin(history, command, info, os.Stdout)
+		return
+	}
+
+	fullPath := getExecutablePath(command)
+
+	if fullPath != "" {
+		cmd := exec.Command(fullPath, info.FinalArgs...)
+		cmd.Args[0] = command
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		outWriter, _ := shell.GetOutputWriter(info.StdoutFile, false, os.Stdout)
+		if info.AppendFile != "" {
+			outWriter, _ = shell.GetOutputWriter(info.AppendFile, true, os.Stdout)
+		}
+		cmd.Stdout = outWriter
+
+		errWriter, _ := shell.GetOutputWriter(info.StderrFile, false, os.Stderr)
+		if info.AppendErrFile != "" {
+			errWriter, _ = shell.GetOutputWriter(info.AppendErrFile, true, os.Stderr)
+		}
+		cmd.Stderr = errWriter
+
+		cmd.Run()
+	} else {
+		fmt.Printf("%s: command not found\n", command)
+	}
+}
